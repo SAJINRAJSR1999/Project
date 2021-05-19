23727251474cb7d0d63453494335c2480932292d
SAJIN RAJ.S.R <84373229+SAJINRAJSR1999@users.noreply.github.com>
2021-05-19 13:02:21 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,14 @@
+function red_btn()
+{
+document.body.style.background="red";
+}
+function green__btn()
+{
+    document.body.style.background="green";
+
+}
+function yellow__btn()
+{
+    document.body.style.background="yellow";
+
+}
